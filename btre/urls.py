8feafe7e23eb223a1e9be607c469a173d702aa70
palakchouhan11 <palakchouhan311@gmail.com>
2023-01-15 23:03:23 +0530

    initial commit

--- btre/urls.py
+++ btre/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from django.conf import settings  #
+from django.conf.urls.static import static  #
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('',include('pages.urls')),
+    path('listings/',include('listings.urls')),
+    path('accounts/',include('accounts.urls')),
+    path('contact/',include('contacts.urls')),
+    path('admin/', admin.site.urls)
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# stati line -> to shoie media folder in frontend
